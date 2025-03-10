718a4716ac30aa3142662fe97aeb9d309c6b9a76
Mercy <owinomercy983@gmail.com>
2025-03-10 18:33:26 +0300

    math lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1; 
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // 14 % 5 = 4
+
+
+var max = Math.max(1, 3, 5, 10, 7, 20)
+console.log(random);
